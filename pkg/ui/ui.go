b96eea89566851639cfa6611c101c3b289ededd2
Marcin Franczyk <mfranczy@redhat.com>
2018-02-02 14:39:26 +0100

    fixed ui paths

--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -3,19 +3,29 @@ package ui
 import (
 	"net/http"
 	"html/template"
+	"runtime"
+	"path"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 )
 
+var basePath string
+
+func init() {
+	_, file, _, _ := runtime.Caller(0)
+	basePath = path.Join(filepath.Dir(file), "layouts")
+}
+
 func layoutRender(rw http.ResponseWriter, req *http.Request) {
 	t := template.New("layoutPage")
-	t, _ = t.ParseFiles("../../pkg/ui/layouts/default.tmpl")
+	t, _ = t.ParseFiles(path.Join(basePath, "default.tmpl"))
 	t.Execute(rw, nil)
 }
 
 func Init(r *mux.Router) {
 	r.HandleFunc("/", layoutRender)
 	r.PathPrefix("/static/").Handler(
-		http.StripPrefix("/static/", http.FileServer(http.Dir("../../pkg/ui/layouts/"))),
+		http.StripPrefix("/static/", http.FileServer(http.Dir(basePath))),
 	)
 }
